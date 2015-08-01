0b910676c4ed3c9482bb6f307dda750373239981
Justin Silverman <justin.silverman@gmail.com>
2015-08-01 16:44:02 -0700

    Setup mocha skeleton

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    mochaTest: {
+      test: {
+        options: {
+          require: ['test/setup.js'],
+          reporter: 'spec',
+          colors: true
+        },
+        src: ['test/**/*.js']
+      }
+    }
   });
+
+  require('load-grunt-tasks')(grunt);
+  grunt.registerTask('default', []);
+  grunt.registerTask('test', ['mochaTest']);
 };
---
+++ test/setup.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.config.includeStack = true;
+
+var sinon = require('sinon');
+require('sinon-as-promised');
+sinon.assert.expose(chai.assert, {
+  prefix: ''
+});
+
+chai.assert.equal = function() {
+  throw new Error("Chai's assert.equal uses ==.  Use assert.strictEqual instead.");
+}
+
+global.assert = chai.assert;
