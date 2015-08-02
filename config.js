09473115767f4fab7fd028e079a623a5a327c41c
Justin Silverman <justin.silverman@gmail.com>
2015-08-01 18:17:52 -0700

    Add empty config file

---
+++ config.js
@@ -0,0 +1,33 @@
+var config = {
+  production: {
+    url: '',
+    database: {
+    },
+    server: {
+      host: '',
+      port: ''
+    }
+  },
+
+  development: {
+    url: '',
+    database: {
+    },
+    server: {
+      host: '',
+      port: ''
+    }
+  },
+
+  test: {
+    url: '',
+    database: {
+    },
+    server: {
+      host: '',
+      port: ''
+    }
+  }
+};
+
+module.exports = config;
