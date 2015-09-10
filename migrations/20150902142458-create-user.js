e2a4a255491616a50841ab1fe2d75a43c5390a33
Justin Silverman <justin.silverman@gmail.com>
2015-09-10 10:37:14 -0700

    Add version to user schema

--- migrations/20150902142458-create-user.js
+++ migrations/20150902142458-create-user.js
@@ -2,15 +2,15 @@
 
 var assign = require('object.assign');
 var timestamps = require('../helpers/timestamps');
-var Schema = require('../schemas/users');
+var initialSchema = require('../schemas/models/users_001');
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.createTable(Schema.tableName,
-      timestamps.addToSchema(Schema.definition(Sequelize)));
+    return queryInterface.createTable(initialSchema.tableName,
+      timestamps.addToSchema(initialSchema.definition(Sequelize)));
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.dropTable(Schema.tableName);
+    return queryInterface.dropTable(initialSchema.tableName);
   }
 };
