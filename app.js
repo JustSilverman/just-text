c97165b7b5ea883bf175cf951f00af365913d988
Justin Silverman <justin.silverman@gmail.com>
2015-09-11 11:23:20 -0700

    Add express scaffolding

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var serveStatic = require('serve-static');
+var morgan = require('morgan');
+var errorhandler = require('errorhandler');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var settings = require('./config/settings');
+
+var app = express();
+
+app.use(morgan('combined'));
+app.use(errorhandler());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(serveStatic(__dirname + '/public'));
+
+app.listen(settings.port, function() {
+  process.stdout.write('Listening on port ' + settings.port + '...');
+});
