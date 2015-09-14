7382c0499887976b548490a8113f535cb3f70adc
Justin Silverman <justin.silverman@gmail.com>
2015-09-14 15:16:52 -0700

    Add dependency: handlebars rendering engine

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var express = require('express');
 var serveStatic = require('serve-static');
 var morgan = require('morgan');
 var errorhandler = require('errorhandler');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var exphbs  = require('express-handlebars');
 
 var settings = require('./config/settings');
 var sessions = require('client-sessions');
@@ -19,6 +22,8 @@ app.use(bodyParser.json());
 app.use(serveStatic(__dirname + '/public'));
 app.use(sessions(settings.session));
 app.use(sessionUpdater);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.listen(settings.port, function() {
   process.stdout.write('Listening on port ' + settings.port + '...');
