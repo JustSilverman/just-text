8db861b3ab2b0009d4e11a242c07697ea8bbbac6
Justin Silverman <justin.silverman@gmail.com>
2015-09-16 00:36:52 -0700

    Add Users controller

--- config/router.js
+++ config/router.js
@@ -2,9 +2,14 @@
 
 var express = require('express');
 var router = express.Router();
+var sessionsHelper = require('../helpers/sessions');
 
 var applicationController = require('../controllers/application');
+var usersController = require('../controllers/users');
 
 router.get('/', applicationController.root);
 
+router.get('/users/new', usersController.newUser);
+router.get('/users/:id', sessionsHelper.requireLogin, usersController.getUser);
+router.post('/users', usersController.getUser);
 module.exports = router;
---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var User  = require('../models').User;
+var sessions = require('../helpers/sessions');
+var toErrorMap = require('../helpers/controller').toErrorMap;
+
+var userController = {
+  newUser: function(req, res) {
+    res.render('signup')
+  },
+
+  getUser: function(req, res) {
+    res.render('profile', { user: req.session.user });
+  },
+
+  createUser: function(req, res) {
+    return User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      password: req.body.password
+    })
+    .then(function(user) {
+      sessions.loginUser(user, req, res);
+      res.redirect('/users/' + user.id);
+    })
+    .catch(function(userResult) {
+      res.render('signup', { errors: toErrorMap(userResult.errors) });
+    });
+  }
+};
+
+module.exports = userController;
